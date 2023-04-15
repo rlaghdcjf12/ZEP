d203a0b495c1b263a9ccfcd2fd1fb454a82b008c
DESKTOP-FN4A2F7\rlagh <rlaghdcjf12@naver.com>
2023-04-15 23:52:40 +0900

    fix noname tag bug

--- seekHANSout/floor5/main.js
+++ seekHANSout/floor5/main.js
@@ -9,7 +9,7 @@ App.onJoinPlayer.Add(function (player) {
   player.tag = {
     sturn: false,
     sTime: 2,
-    condition: ['noName'],
+    condition: [conditionTag[0]],
     noteStatus: {
       flowNo: 0,
       finishPage: -1,
@@ -32,19 +32,19 @@ App.onObjectTouched.Add(function (sender, x, y, tileID, obj) {
     };
     dialogObject = handleObjectScenario(sender, dialogObject);
 
-    if (dialogObject.type == 0 && sender.tag.condition.includes('noName')) openSetNameDialog(sender);
+    if (dialogObject.type == 0 && checkCondition(sender, conditionTag[0])) openSetNameDialog(sender);
     else openDialog(sender, dialogObject);
   }
 });
 
 App.addOnLocationTouched('startLimit', function (player) {
-  if (player.tag.condition.includes('noName')) {
+  if (checkCondition(player, conditionTag[0])) {
     player.spawnAtLocation('startLimitRespawn', 4);
     openToast(player, '쟝을 먼저 만나보자.');
   }
 });
 App.addOnLocationTouched('waterFall', function (player) {
-  if (player.tag.condition.includes('hansNote') && [6, 7].includes(player.tag.listNo)) {
+  if (checkCondition(player, conditionTag[4]) && [6, 7].includes(player.tag.listNo)) {
     player.tag.listNo == 8;
     setCheckList(player, 8);
   }
@@ -58,30 +58,32 @@ App.addOnLocationTouched('coin', function (player) {
   openDialog(player, { type: 1, id: 42 });
 });
 // App.addOnLocationTouched('finish', function (player) {
-//   if (!player.tag.condition.includes('finish')) {
-//     player.tag.condition.push('finish');
+//   if (!checkCondition(player, conditionTag[100])) {
+//     addCondition(player, 'finish');
 //     player.tag.widgetTimer.destroy();
 //     player.tag.widgetTimer = null;
 //   }
 // });
 
 App.addOnKeyDown(27, function (player) {
-  if (player.tag.widgetDialog != null) {
-    closeDialog(player);
-  }
+  if (player.tag.widgetDialog != null) closeDialog(player);
 });
 
 const handleObjectScenario = (player, { type, id }) => {
+  if (type == 0) {
+    if (!checkCondition(player, conditionTag[0])) return { type: 1, id };
+  }
   // 1. normal
-  if (type == 1) {
-    if (id == 10 && !player.tag.condition.includes('help')) player.tag.condition.push('help'); // 가비 화물 도움 태그
+  else if (type == 1) {
+    if (id == 10 && !checkCondition(player, conditionTag[1]))
+      addCondition(player, conditionTag[1]); // 가비 화물 도움 태그
     else if (id == 22 && player.tag.listNo == 6) setCheckList(player, 7); // 여사님 대화: 체크리스트 6->7
-    else if (id == 40 && !player.tag.condition.includes('toilet')) player.tag.condition.push('toilet');
-    else if (id == 42 && !player.tag.condition.includes('coin')) player.tag.condition.push('coin');
+    else if (id == 40 && !checkCondition(player, conditionTag[2])) addCondition(player, conditionTag[2]);
+    else if (id == 42 && !checkCondition(player, conditionTag[99])) addCondition(player, conditionTag[99]);
   }
   // 2. input
   else if (type == 2) {
-    if (id == 0 && player.tag.condition.includes('hansNote')) return { type: 1, id: 7 }; // SE 보관함 오픈: input->normal
+    if (id == 0 && checkCondition(player, conditionTag[4])) return { type: 1, id: 7 }; // SE 보관함 오픈: input->normal
   }
 
   return { type, id };
@@ -97,7 +99,7 @@ const handleDialogMessage = (player, msg) => {
     if (msg.listNo == 8) {
       openToast(player, '한스의 노트를 다시 확인해보자.', 2);
       setCheckList(player, 9);
-      player.tag.condition.push('Complete1');
+      addCondition(player, conditionTag[5]);
       player.tag.condition.noteStatus = {
         flowNo: 1,
         finishPage: -1,
@@ -107,8 +109,7 @@ const handleDialogMessage = (player, msg) => {
   } else if (msg.type == 'openToast') {
     openToast(player, msg.toast, 2);
   } else if (msg.type == 'addCondition') {
-    log(`[handleDialogMessage] condition : ${msg.condition}`);
-    player.tag.condition.push(msg.condition);
+    addCondition(player, conditionTag[msg.condition]);
   } else if (msg.type == 'openHansNote') {
     openNoteButton(player);
   } else if (msg.type == 'saveHansNote') {
@@ -118,6 +119,15 @@ const handleDialogMessage = (player, msg) => {
     player.tag.widgetDialog = null;
     openDialog(player, { type: msg.dialogType, id: msg.link });
   }
+};
+
+const checkCondition = (player, tag) => {
+  return player.tag.condition.includes(tag);
+};
+
+const addCondition = (player, tag) => {
+  log(`addCondition tag : ${tag}`);
+  player.tag.condition.push(tag);
 };
 
 // TODO: 추후 지울 것
@@ -221,7 +231,7 @@ const closeDialog = (player) => {
   player.tag.widgetDialog = null;
   player.tag.widgetDialogType = null;
 
-  if (player.tag.condition.includes('hansNote') && !player.tag.widgetNoteButton) openNoteButton(player);
+  if (checkCondition(player, conditionTag[4]) && !player.tag.widgetNoteButton) openNoteButton(player);
 };
 
 const openNoteButton = (player) => {
@@ -272,6 +282,17 @@ const dialogSize = {
   2: [20, 5, 20, 5, 20, 25, 20, 25],
   3: [20, 5, 20, 5, 20, 30, 20, 30],
   4: [20, 5, 20, 5, 20, 30, 20, 30],
+};
+
+const conditionTag = {
+  0: 'noName',
+  1: 'help',
+  2: 'toilet',
+  3: 'openSE',
+  4: 'hansNote',
+  5: 'Complete1',
+  99: 'coin',
+  100: 'finish',
 };
 
 const TEXT_SPEED_UP_COFFEE = '커피를 마셨더니 몸이 가벼워진 것 같다.';
